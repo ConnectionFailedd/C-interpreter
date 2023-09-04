01fc587bdb59ef12d31a96c44feb53d7ee972164
__NYA__ <squirrel@mail.ustc.edu.cn>
2023-09-04 11:36:34 +0800

    little bug fixed

--- utils/name.cpp
+++ utils/name.cpp
@@ -6,6 +6,7 @@ namespace CINT {
 Match Name::operator==(const UnconfirmedName & _rhs) const & noexcept {
     if(__baseName != _rhs.base_name()) return Match::Status::NOT_MATCHED;
     if(_rhs.specified_scope() != Scope::NOSCOPE && _rhs.specified_scope() == this->__scope) return Match::Status::SPECIFIED_MATCHED;
+    if(_rhs.specified_scope() != Scope::NOSCOPE) return Match::Status::NOT_MATCHED;
 
     for(auto iter = Scope::currentScopeStack.rbegin(); iter != Scope::currentScopeStack.rend(); iter ++) {
         if(*iter == this->__scope) return Match::Status::LOCAL_MATCHED;
